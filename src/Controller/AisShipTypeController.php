64673e4fad9d30d66258ae49ae28a933f3ba8f5d
Gabriel DELAGE <delage.gabriel83@gmail.com>
2020-12-09 16:12:15 +0100

    màj

--- src/Controller/AisShipTypeController.php
+++ src/Controller/AisShipTypeController.php
@@ -6,13 +6,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/aisshiptype", name"aisshiptype_")
+ */
 class AisShipTypeController extends AbstractController {
 
     /**
-     * @Route("/ais/ship/type", name="ais_ship_type")
+     * @Route("/aisshiptype", name="aisshiptype")
      */
     public function index(): Response {
-        return $this->render('ais_ship_type/index.html.twig', [
+        return $this->render('aisshiptype/index.html.twig', [
                     'controller_name' => 'AisShipTypeController',
         ]);
     }
--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -5,25 +5,45 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Service\GestionContact;
 
-class MessageController extends AbstractController
-{
+/**
+ * @Route("/message", name"messagee_")
+ */
+class MessageController extends AbstractController {
+
     /**
      * @Route("/message", name="message")
      */
-    public function index(): Response
-    {
+    public function index(): Response {
         return $this->render('message/index.html.twig', [
-            'controller_name' => 'MessageController',
+                    'controller_name' => 'MessageController',
         ]);
     }
-    
+
     /**
      * @Route("/contact", name="contact")
      */
-    public function contact() {
+    public function contact(Request $request, GestionContact $gestionContact): Response {
+        $message = new Message();
+
+        $form->$this->createForm(MessageType::class, $message);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $message = $form->getData();
+
+            $gestionContact->envoiMailContact($message);
+
+
+            $this->addFlash('notification', "Votre message a bien été envoyé");
+            return $this->redirectToRoute("home");
+        }
+
         return $this->render('message/contact.html.twig', [
-            'form_name' => 'MessageType',
+                    'form' => $form->createView(),
         ]);
     }
+
 }
